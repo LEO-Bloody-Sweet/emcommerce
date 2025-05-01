7375410b0ce60c3ddda96d9e3afe71758ef14d95
LEO-Bloody-Sweet <145244508+LEO-Bloody-Sweet@users.noreply.github.com>
2025-05-01 12:54:28 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import "./globals.css"
+
+export default function Home() {
+  return (
+    <div className="bg-amber-900">
+      <p className="text-rose-500 bg-black">Hello World</p>
+      <h1 className="text-green-600">Sultan</h1>
+      <p className="text-rose"></p>
+    </div>
+  )
+}
